2138adbef11e7d7c21a99fb2b46eb6eca696a863
mike7fnd <mikefernandex227@gmail.com>
2025-02-25 19:36:46 +0800

    Create sketch_feb23a.ino

---
+++ ACTIVITY_1/sketch_feb23a.ino
@@ -0,0 +1,23 @@
+#include <WiFi.h>
+const char* ssid = "0000";
+const char* password = "123456788";
+void setup() {
+  // Start Serial communication
+  Serial.begin(115200);
+
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+  Serial.println("Connected to WiFi");
+
+  // Print the IP address
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+
+}
+
